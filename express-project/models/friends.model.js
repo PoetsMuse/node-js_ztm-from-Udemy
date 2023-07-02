4a8579df3755a869696e7d7554e113752e90c019
Kate Glushkova <weirdis1@gmail.com>
2023-07-02 14:38:57 +0300

    Working with Postman, Express, nodemon, npm. Started MVC / Model, View, Controller

---
+++ express-project/models/friends.model.js
@@ -0,0 +1,12 @@
+const friends = [
+    {
+        id: 0,
+        name: 'John Snow',
+    },
+    {
+        id: 1,
+        name: 'Baba Yagaga',
+    },
+];
+
+module.exports = friends;
--- express-project/server.js
+++ express-project/server.js
@@ -1,44 +1,27 @@
 const express = require('express');
+const friendsController = require('./controllers/friends.controller');
+const messagesController = require('./controllers/messages.controller');
 
 const app = express();
 
 const PORT = 3000;
 
-const friends = [
-    {
-        id: 0,
-        name: 'John Snow',
-    },
-    {
-        id: 1,
-        name: 'Baba Yaga',
-    },
-];
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`)
+});
 
-app.get('/friends', (req, res) => {
-    res.json(friends);
-})
+app.use(express.json());
 
+app.post('/friends', friendsController.postFriend);
+app.get('/friends', friendsController.getFriends);
 //GET /friends/id
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.status(200).json(friend);
-    }else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        });
-    }
-});
+app.get('/friends/:friendId', friendsController.getFriend);
 
-app.get('/messages', (req, res) => {
-    res.send('<ul><li>Hey there!</li></ul>');
-});
-
-app.post('/messages', (req, res) => {
-    console.log('Updating messages...');
-})
+app.get('/messages', messagesController.getMessages);
+app.post('/messages', messagesController.postMessage);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
