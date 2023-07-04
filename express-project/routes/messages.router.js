ea0f7f034e227359dd6c94cdac857b4f1b1ed80a
Kate Glushkova <weirdis1@gmail.com>
2023-07-04 20:02:05 +0300

    Routes added

---
+++ express-project/routes/messages.router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const messagesController = require('../controllers/messages.controller');
+
+
+const messagessRouter = express.Router();
+
+messagessRouter.get('/', messagesController.getMessages);
+messagessRouter.post('/', messagesController.postMessage);
+
+module.exports = messagessRouter;
+
+
