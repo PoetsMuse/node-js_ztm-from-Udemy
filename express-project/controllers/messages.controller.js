6346c81b6f879ef6b2f0b30dc0c013e31c0fafdd
Kate Glushkova <weirdis1@gmail.com>
2023-07-04 20:34:06 +0300

    Send file from public folder

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path  = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hey there!</li></ul>');
+    res.sendFile(path.join(__dirname, '..', 'public', 'smile.png'));
+    // res.send('<ul><li>Hey there!</li></ul>');
 }
 
 function postMessage(req, res) {
