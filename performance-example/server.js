f6eb15c5faebc7b28beeb051493b4ddeb3310f24
Kate Glushkova <weirdis1@gmail.com>
2023-08-05 17:55:45 +0300

    Clustering 1

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
@@ -18,13 +19,24 @@ app.get('/', (req, res) => {
     // JSON.parse("{}") => {}
     //[5, 9, 7, 6, 8].sort()
     //key derivation functions crypto...
-    res.send('Performance example');
+    res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
     //delay the response
     delay(9000);
-    res.send('Ring! Ring! Ring!');
+    res.send(`Ring! Ring! Ring! ${process.pid}`);
 });
 
-app.listen(3000);
+//for master and forks the same file and code is run: server.js
+console.log('Running server.js...');
+if (cluster.isMaster){
+    console.log('Master has been started...');
+    cluster.fork();
+    cluster.fork();
+} else {
+    console.log('Worker process started...');
+    app.listen(3000);
+}
+
+
